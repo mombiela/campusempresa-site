eec8de220e62c482f6f26a5a0254f7bd6ec0a2d5
Joan Costa Mombiela <jcostamo@fiatc.es>
2024-08-29 12:34:33 +0200

    M+

--- generate.bat
+++ generate.bat
@@ -1,3 +1,16 @@
+@echo off
+setlocal
+
+REM EJECUCIÓN PRINCIPAL
 cd %~dp0
 java -cp bin;lib/*; org.swb.Executor processor.properties main
+REM FIN EJECUCIÓN PRINCIPAL
 
+if %errorlevel% neq 0 (
+    echo Ha ocurrido un error. Presiona una tecla para continuar...
+    pause >nul
+) else (
+    echo Script completado exitosamente.
+)
+
+endlocal
