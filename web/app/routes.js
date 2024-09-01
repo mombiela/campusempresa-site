b0437f4611c7c7a06bf4207c74948457c61c4793
Joan Costa <joan.costa.mombiela@gmail.com>
2024-09-01 19:26:28 +0200

    M+

--- web/app/routes.js
+++ web/app/routes.js
@@ -1,3 +1,12 @@
+/* *********************************************
+   Las routes van de més específiques a menys!!!
+   Exemple:
+   
+   aaa/bbb/ccc
+   aaa/bbb
+   aaa
+// *********************************************/
+
 export const ROUTES = [
 	{src:"index/", dst:"github/mombiela/campusempresa/", stage:"prod"},
 	{src:"tic/", dst:"github/mombiela/campusempresa/", stage:"prod"},
