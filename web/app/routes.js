422150e83f03735ccceeaa942cbafea6fba87592
Joan Costa Mombiela <jcostamo@fiatc.es>
2024-09-02 12:22:20 +0200

    M+

--- web/app/routes.js
+++ web/app/routes.js
@@ -1,31 +1,35 @@
-/* *********************************************
-   Las routes van de més específiques a menys!!!
-   Exemple:
-   
-   aaa/bbb/ccc
-   aaa/bbb
-   aaa
-// *********************************************/
+const ROUTES = {};
 
-const ROUTES = [
-	{src:"tic/react/", dst:"github/mombiela/campusempresa/react/", stage:"prod"},
-	{src:"tic/html/", dst:"github/mombiela/campusempresa/html/", stage:"test"},
-	{src:"tic/", dst:"github/mombiela/campusempresa/", stage:"pre"},
-	{src:"about", dst:"about", stage:"pre"},
-	{src:"donate", dst:"donate", stage:"pre"},
-	{src:"contribute", dst:"contribute", stage:"pre"},
-	{src:"licence", dst:"licence", stage:"pre"},
-	{src:"index", dst:"index", stage:"pre"},
-]
+// Rutes directes
+ROUTES["about"] 		= {dst:"about", stage:"pre"};
+ROUTES["donate"] 		= {dst:"donate", stage:"pre"};
+ROUTES["contribute"] 	= {dst:"contribute", stage:"pre"};
+ROUTES["licence"] 		= {dst:"licence", stage:"pre"};
+ROUTES["index"] 		= {dst:"index", stage:"pre"};
+
+// Rutas con prefix (Las routes van de més específiques a menys!!!)
+// Exemple: aaa/bbb/ccc, aaa/bbb, aaa
+ROUTES["tic/react/"] 	= {dst:"github/mombiela/campusempresa/react/", stage:"prod", prefix:true};
+ROUTES["tic/html/"] 	= {dst:"github/mombiela/campusempresa/html/", stage:"test", prefix:true};
+ROUTES["tic/"] 			= {dst:"github/mombiela/campusempresa/", stage:"pre", prefix:true};
+
+for (const property in ROUTES)
+{
+	let route = ROUTES[property];
+	route.src = property;
+}
 
 export function getRouteInfo(url)
 {
 	if (url.startsWith("#")) url = url.substring(1);
 	
-	// Miramos si es una url de cambio
-	for (let i = 0; i<ROUTES.length; i++)
+	if (ROUTES[url]) return ROUTES[url];
+	
+	// Miramos si es una url de cambio con prefix
+	for (const property in ROUTES)
 	{
-		let route = ROUTES[i];
+		let route = ROUTES[property];
+		if (!route.prefix) continue;
 		if (url.startsWith(route.src))
 		{
 			return route;
