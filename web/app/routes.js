1479fc68cfabc86d8ae67b9d2ab45175ccef91cf
Joan Costa <joan.costa.mombiela@gmail.com>
2024-09-02 18:02:55 +0200

    M+

--- web/app/routes.js
+++ web/app/routes.js
@@ -1,12 +1,21 @@
 const ROUTES = {};
 
 // Rutes directes
-ROUTES["about"] 		= {dst:"about", stage:"pre"};
-ROUTES["donate"] 		= {dst:"donate", stage:"pre"};
-ROUTES["contribute"] 	= {dst:"contribute", stage:"pre"};
-ROUTES["licence"] 		= {dst:"licence", stage:"pre"};
-ROUTES["index"] 		= {dst:"index", stage:"pre"};
-ROUTES["_default"] 		= {dst:"_default", stage:"pre"};
+ROUTES["about"] 		= {stage:"pre"};
+ROUTES["donate"] 		= {stage:"pre"};
+ROUTES["contribute"] 	= {stage:"pre"};
+ROUTES["licence"] 		= {stage:"pre"};
+ROUTES["index"] 		= {stage:"pre"};
+ROUTES["_default"] 		= {stage:"pre"};
+
+// Rutas TIC
+ROUTES["tic/prog_frontend"] = {stage:"pre"};
+ROUTES["tic/prog_backend"]  = {stage:"test"};
+ROUTES["tic/prog_legacy"]   = {stage:"test"};
+ROUTES["tic/prog_full_stack"]   = {stage:"test"};
+ROUTES["tic/arq_web"]   = {stage:"test"};
+ROUTES["tic/arq_cloud"]   = {stage:"test"};
+ROUTES["tic/exp_seg"]   = {stage:"test"};
 
 // Rutas con prefix (Las routes van de més específiques a menys!!!)
 // Exemple: aaa/bbb/ccc, aaa/bbb, aaa
@@ -18,6 +27,7 @@ for (const property in ROUTES)
 {
 	let route = ROUTES[property];
 	route.src = property;
+	if (!route.dst) route.dst = property;
 }
 
 export function getRouteInfo(url)
