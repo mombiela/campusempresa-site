6916189aad24e387b5e82d2f032ba91cd3c9481c
Joan Costa <joan.costa.mombiela@gmail.com>
2024-09-02 16:18:09 +0200

    M+

--- web/app/literals.js
+++ web/app/literals.js
@@ -1,6 +1,6 @@
 const LITERALS = {
 	"ca":{
-		"about":"Qui som",
+		"about":"Sobre nosaltres",
 		"donate":"Donacions",
 		"contribute":"Contribuir",
 		"licence":"Llicència",
@@ -8,24 +8,25 @@ const LITERALS = {
 		"objective": "El Projecte",
 		"slogan":"<cite>Construint la societat d'avui i del demà</cite>",
 		"publi1": "Publicitat",
-		"publi2": "Aquest espai está destinat a publicitat.",
-		"publi3": "Si vols ser un patrocinador nostre <a href='#contact'>contacta amb nosaltres</a> i podràs incloure enllaços en aquesta zona.",
-		"publi4": "Grècias per col·laborar!"
+		"publi2": "Aquest espai està destinat a publicitat.",
+		"publi3": "Si vols ser patrocinador, <a href='#contact'>contacta'ns</a> per incloure enllaços en aquesta zona.",
+		"publi4": "Gràcies per col·laborar!"
 	},
 	"es":{
-		"about":"Quienes somos",
+		"about":"Sobre nosotros",
 		"donate":"Donaciones",
 		"contribute":"Contribuir",
 		"licence":"Licencia",
 		"copyright":"Todos los derechos reservados",
 		"objective": "El Proyecto",
-		"slogan":"<cite>Construyendo la socidead de hoy y del mañana</cite>",
+		"slogan":"<cite>Construyendo la sociedad de hoy y del mañana</cite>",
 		"publi1": "Publicidad",
 		"publi2": "Este espacio está destinado a publicidad.",
-		"publi3": "Si quieres ser un patrocinador nuestro <a href='#contact'>contacta con nosotros</a> y podrás incluir enlaces en esta zona.",
+		"publi3": "Si quieres ser patrocinador, <a href='#contact'>contáctanos</a> para incluir enlaces en esta zona.",
 		"publi4": "¡Gracias por colaborar!"
 	}
 };
+
 
 export function getLiterals(lang)
 {
