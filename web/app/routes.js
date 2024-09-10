a29e996e36b9ffe1a9c8e66cfaeb1775de54dd2b
Joan Costa <joan.costa.mombiela@gmail.com>
2024-09-10 22:15:21 +0200

    M+

--- web/app/routes.js
+++ web/app/routes.js
@@ -9,19 +9,10 @@ ROUTES["index"] 		= {stage:"pre"};
 ROUTES["objective"]		= {stage:"pre"};
 ROUTES["_default"] 		= {stage:"pre"};
 
-// Rutas TIC
-ROUTES["tic/prog_frontend"] = {stage:"pre"};
-ROUTES["tic/prog_backend"]  = {stage:"test"};
-ROUTES["tic/prog_legacy"]   = {stage:"test"};
-ROUTES["tic/prog_full_stack"]   = {stage:"test"};
-ROUTES["tic/arq_web"]   = {stage:"test"};
-ROUTES["tic/arq_cloud"]   = {stage:"test"};
-ROUTES["tic/exp_seg"]   = {stage:"test"};
-
 // Rutas con prefix (Las routes van de més específiques a menys!!!)
 // Exemple: aaa/bbb/ccc, aaa/bbb, aaa
-ROUTES["tic/react/"] 	= {dst:"github/mombiela/campusempresa/react/", stage:"prod", prefix:true};
-ROUTES["tic/html/"] 	= {dst:"github/mombiela/campusempresa/html/", stage:"test", prefix:true};
+ROUTES["tic/react/"] 	= {dst:"github/mombiela/campusempresa/react/", stage:"test", prefix:true};
+ROUTES["tic/html/"] 	= {dst:"github/mombiela/campusempresa/html/", stage:"prod", prefix:true};
 ROUTES["tic/"] 			= {dst:"github/mombiela/campusempresa/", stage:"pre", prefix:true};
 
 for (const property in ROUTES)
