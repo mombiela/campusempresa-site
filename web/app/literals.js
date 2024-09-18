8f70223cad2d0436e1a86649f0c9afefc18e376d
Joan Costa <joan.costa.mombiela@gmail.com>
2024-09-18 19:39:30 +0200

    M+

--- web/app/literals.js
+++ web/app/literals.js
@@ -9,7 +9,7 @@ const LITERALS = {
 		"slogan":"<cite>Construint la societat d'avui i del demà</cite>",
 		"publi1": "Publicitat",
 		"publi2": "Aquest espai està destinat a publicitat.",
-		"publi3": "Si vols ser patrocinador, <a href='#about'>contacta'ns</a> per incloure enllaços en aquesta zona.",
+		"publi3": "Si vols ser patrocinador, contacta amb nosaltres per incloure enllaços en aquesta zona: <a href='mailto:admin@campusempresa.cat'>admin@campusempresa.cat</a> ",
 		"publi4": "Gràcies per col·laborar!"
 	},
 	"es":{
@@ -22,7 +22,7 @@ const LITERALS = {
 		"slogan":"<cite>Construyendo la sociedad de hoy y del mañana</cite>",
 		"publi1": "Publicidad",
 		"publi2": "Este espacio está destinado a publicidad.",
-		"publi3": "Si quieres ser patrocinador, <a href='#about'>contáctanos</a> para incluir enlaces en esta zona.",
+		"publi3": "Si quieres ser patrocinador, contáctanos para incluir enlaces en esta zona: <a href='mailto:admin@campusempresa.cat'>admin@campusempresa.cat</a>",
 		"publi4": "¡Gracias por colaborar!"
 	}
 };
